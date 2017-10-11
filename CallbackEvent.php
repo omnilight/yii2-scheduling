b5d0956da7b0e5676914067c635baa7787520e5c
Sergey Kasatkin <spam@onsky.ru>
2017-10-11 11:11:55 +0300

    Fixes #17: Use `Yii::configure` instead of static call to `yii\base\Object` constructor

--- CallbackEvent.php
+++ CallbackEvent.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace omnilight\scheduling;
+use Yii;
 use yii\base\Application;
 use yii\base\InvalidParamException;
-use yii\base\Object;
-
 
 /**
  * Class CallbackEvent
@@ -34,7 +33,9 @@ class CallbackEvent extends Event
         $this->callback = $callback;
         $this->parameters = $parameters;
 
-        Object::__construct($config);
+        if (!empty($config)) {
+            Yii::configure($this, $config);
+        }
 
         if ( ! is_string($this->callback) && ! is_callable($this->callback))
         {
